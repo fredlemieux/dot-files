7028f9dd7d5094d196b576733d444fd1c834d769
Frederique Lemieux <fredlemi@gmail.com>
2026-02-10 00:57:12 +0100

    Add experiments file for trial apps

---
+++ install/60-experiments.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# These are experiments, apps, that I'm not sure I want to keep.
+# When they become stable, they should be moved to the correct install files.
+
+# Experiments with multi-agent workflows
+# https://app.factory.ai/
+#curl -fsSL https://app.factory.ai/cli | sh
+# NOTE also installed the desktop app
+brew install --cask droid
+
