64bed83702c98c3447878759ffb5498f2e9c54ab
Frederique Lemieux <fredlemi@gmail.com>
2026-02-09 12:08:28 +0100

    Add pnpm setup to the languages-work file

--- install/41-languages-work.sh
+++ install/41-languages-work.sh
@@ -1,4 +1,8 @@
 # Current projects specific language tools
-pyenv install 3.9
-pyenv install 3.11
-pyenv install 3.12
+pyenv install --skip-existing 3.9
+pyenv install --skip-existing 3.11
+pyenv install --skip-existing 3.12
+
+# Enable corepack and install pnpm
+corepack enable
+corepack prepare pnpm@latest --activate
